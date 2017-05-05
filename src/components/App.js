6599d45f9fbe04a6f7358cdd804c50b4b2d9fc77
greggpatton <gregg.patton@gmail.com>
2017-05-05 09:35:14 -0500

    Changes for standarjs coding

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import '../assets/stylesheets/base.scss';
-
+import '../assets/stylesheets/base.scss'
 
 const App = ({ name }) => {
   return (
     <h1>Hello, {name}!</h1>
-  );
-};
+  )
+}
 
 App.propTypes = {
-  name: PropTypes.string,
-};
+  name: PropTypes.string
+}
 
-export default App;
+export default App
