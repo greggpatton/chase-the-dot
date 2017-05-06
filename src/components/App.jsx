f8cfab49f4ae732899ad71c66b2f799cf948190e
greggpatton <gregg.patton@gmail.com>
2017-05-05 20:02:45 -0500

    Building components

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import Navigation from './Navigation'
+
+export default class App extends Component {
+  // state = {  }
+  render () {
+    return (
+      <Navigation />
+    )
+  }
+}
+
+// import React from 'react'
+// import PropTypes from 'prop-types'
+
+// import '../assets/stylesheets/base.scss'
+
+// const App = ({ name }) => {
+//   return (
+//     <h1>Hello there {name}!</h1>
+//   )
+// }
+
+// App.propTypes = {
+//   name: PropTypes.string
+// }
+
+// export default App
